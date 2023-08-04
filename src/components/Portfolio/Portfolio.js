ca12148ab84169fb8990a0159b57fe5c9dcdfa66
dv-loginov <dilog@rambler.ru>
2023-08-04 16:54:47 +0300

    feat: add section Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,24 @@ import './Portfolio.css';
 const Portfolio = () => {
   return (
     <div className='portfolio'>
-
+      <h2 className="portfolio__header">Портфолио</h2>
+      <ul className='portfolio__list'>
+        <li className="portfolio__item">
+          <a href="/" className="portfolio__link">Статичный сайт
+            <div className="portfolio__ico">↗</div>
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="/" className="portfolio__link">Адаптивный сайт
+            <div className="portfolio__ico">↗</div>
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="/" className="portfolio__link">Одностраничное приложение
+            <div className="portfolio__ico">↗</div>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };

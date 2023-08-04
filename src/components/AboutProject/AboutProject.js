dba766590730fec3626ad72579882ef2154d39bd
dv-loginov <dilog@rambler.ru>
2023-08-04 14:07:16 +0300

    feat: add multiple sections

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,13 +1,39 @@
 import './AboutProject.css';
+import HeaderSection from '../HeaderSection/HeaderSection';
 
 const AboutProject = () => {
   return (
-    <section className='about-project'>
-      <div className="about-project__container">
-
+    <div className="about-project">
+      <HeaderSection name='О проекте'/>
+      <div className="about-project__cards">
+        <div className="about-project__card">
+          <h3 className="about-project__card-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <h4 className="about-project__card-subtitle">
+            Составление плана, работу над бэкендом, вёрстку,
+            добавление функциональности и финальные доработки.
+          </h4>
+        </div>
+        <div className="about-project__card">
+          <h3 className="about-project__card-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <h4 className="about-project__card-subtitle">
+            У каждого этапа был мягкий и жёсткий дедлайн,
+            которые нужно было соблюдать, чтобы успешно защититься.
+          </h4>
+        </div>
       </div>
-
-    </section>
+      <div className="about-project__lasting">
+        <div className="about-project__cell about-project__cell_short about-project__cell_colored">1 неделя</div>
+        <div className="about-project__cell">4 недели</div>
+      </div>
+      <div className="about-project__naming">
+        <div className="about-project__cell about-project__cell_short">Back-end</div>
+        <div className="about-project__cell">Front-end</div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,10 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Section from '../Section/Section'
 import Promo from '../Promo/Promo';
-// import AboutProject from '../AboutProject/AboutProject';
-
-
-
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 const Main = () => {
   return (
@@ -17,9 +17,19 @@ const Main = () => {
           <Promo />
         </Section>
         <Section name = 'about-project'>
+          <AboutProject />
         </Section>
-        {/*<Promo />*/}
-        {/*<AboutProject />*/}
+        <Section name = 'techs'>
+          <Techs />
+        </Section>
+        <Section name = 'about-me'>
+          <AboutMe />
+        </Section>
+        <Section name = 'portfolio'>
+          <Portfolio />
+        </Section>
+
+
       </main>
       <Footer />
     </>

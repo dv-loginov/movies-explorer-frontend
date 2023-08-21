98f0c46b30f900775a89c9ce5b04fbdf70eb4c8c
dv-loginov <dilog@rambler.ru>
2023-08-21 15:34:00 +0300

    feat: protect routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import PageMain from '../PageMain/PageMain';
 import PageMovies from '../PageMovies/PageMovies';
 import PageSavedMovies from '../PageSavedMovies/PageSavedMovies';
@@ -9,24 +9,76 @@ import PageLogin from '../PageLogin/PageLogin';
 import PageRegister from '../PageRegister/PageRegister';
 import Page404 from '../Page404/Page404';
 import { CurrentUserContext } from '../../context/CurrentUserContext';
+import MainApi from '../../utils/MainApi';
+import ProtectedRoute from '../../utils/ProtectedRoute';
 
 const App = () => {
+  // const navigate = useNavigate();
+  // const location = useLocation();
 
   const [currentUser, setCurrentUser] = useState({});
+  const [isLoggedIn, setLoggedIn] = useState(null);
+
+  const checkToken = () => {
+    console.log('проверка токена');
+    MainApi.getUser()
+      .then((user) => {
+        setLoggedIn(true);
+        setCurrentUser(user);
+        // navigate(location.pathname);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoggedIn(false);
+      });
+  }
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      MainApi.getUser()
+        .then((user) => {
+          setCurrentUser(user);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [isLoggedIn]);
 
   return (
     <CurrentUserContext.Provider value={ currentUser }>
-    <div className='app'>
-      <Routes>
-        <Route path='/' element={ <PageMain/> }/>
-        <Route path='/movies' element={ <PageMovies/> }/>
-        <Route path='/saved-movies' element={ <PageSavedMovies/> }/>
-        <Route path='/profile' element={ <PageProfile/> }/>
-        <Route path='/signin' element={ <PageLogin/> }/>
-        <Route path='/signup' element={ <PageRegister/> }/>
-        <Route path='*' element={ <Page404/> }/>
-      </Routes>
-    </div>
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={ <PageMain isLoggedIn={ isLoggedIn }/> }/>
+          <Route path='/signin' element={ <PageLogin/> }/>
+          <Route path='/signup' element={ <PageRegister/> }/>
+
+          <Route path='/movies' element={
+            <ProtectedRoute
+              element={ <PageMovies /> }
+              isLoggedIn={ isLoggedIn }
+            />
+          }/>
+
+          <Route path='/saved-movies' element={
+            <ProtectedRoute
+              element={ <PageSavedMovies /> }
+              isLoggedIn={ isLoggedIn }
+            />
+          }/>
+
+          <Route path='/profile' element={
+            <ProtectedRoute
+              element={ <PageProfile/> }
+              isLoggedIn={ isLoggedIn }
+            />
+          }/>
+
+          <Route path='*' element={ <Page404/> }/>
+        </Routes>
+      </div>
     </CurrentUserContext.Provider>
   );
 };
---
+++ src/utils/MainApi.js
@@ -0,0 +1,66 @@
+class MainApi {
+  constructor(opt) {
+    this._baseUrl = opt.baseUrl;
+  }
+
+  // register({password, email}) {
+  //   return this._request('signup', {
+  //     method: 'POST',
+  //     headers: {"Accept": "application/json", "Content-Type": "application/json"},
+  //     credentials: 'include',
+  //     body: JSON.stringify({
+  //       password: password,
+  //       email: email,
+  //     })
+  //   });
+  // }
+  //
+  // authorize({password, email}) {
+  //   return this._request('signin', {
+  //     method: 'POST',
+  //     headers: {"Accept": "application/json", "Content-Type": "application/json"},
+  //     credentials: 'include',
+  //     body: JSON.stringify({
+  //       password: password,
+  //       email: email,
+  //     })
+  //   });
+  // }
+  //
+  // exit() {
+  //   return this._request('signout', {
+  //     method: 'POST',
+  //     headers: {"Accept": "application/json", "Content-Type": "application/json"},
+  //     credentials: 'include',
+  //   });
+  // }
+
+
+  getUser() {
+    return this._request('users/me', {
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: 'include',
+    });
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${ res.status }`);
+    }
+    return res.json();
+  }
+
+  _request(endpoint, options) {
+    return fetch(`${ this._baseUrl }/${ endpoint }`, options).then(this._getResponseData)
+  }
+
+}
+
+const mainApi = new MainApi({
+  baseUrl: 'http://localhost:3005',
+});
+
+export default mainApi ;
